2ff8746e7e5bdbbea1ec1885e7d9a43a07e373f9
Mayank-44 <37243315+Mayank-44@users.noreply.github.com>
2018-03-11 10:24:22 +0530

    array

--- Data Structures/reverse_array.cpp
+++ Data Structures/reverse_array.cpp
@@ -1,33 +1,16 @@
-<<<<<<< HEAD
 #include<iostream>
 using namespace std;
 int main()
 {
-	int n;			\\size of array
+	int n;			//size of array
 	cin>>n;
 	
 	int ar[n];
-	for(int i=0;i<n;i++)
+	for(int i=0;i<n;i++)  //input array elements
 	cin>>ar[i];
 	
 	for(int i=n-1;i>=0;i--)
 	cout<<ar[i];
 	
 }
-=======
-#include<iostream>
-using namespace std;
-int main()
-{
-	int n;			\\size of array
-	cin>>n;
-	
-	int ar[n];
-	for(int i=0;i<n;i++)
-	cin>>ar[i];
-	
-	for(int i=n-1;i>=0;i--)
-	cout<<ar[i];
-	
-}
->>>>>>> 9497e38bce335950fdb2a848b9a70b08e1e8cb40
+
